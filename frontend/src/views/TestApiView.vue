258b7fa800f819434760edcde231f93cb8e7aec9
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2023-04-03 13:09:17 -0300

    api: testing connection between api x laravel x vue

---
+++ frontend/src/views/TestApiView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    test api
+    <div v-for="game in games" :key="game.id">
+      <h3>{{ game.name }}</h3>
+      <p>desc: {{ game.description }}</p>
+      <img :src="game.background_image" alt="Capa do jogo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "TestApiView",
+  data() {
+    return {
+      games: [],
+    };
+  },
+  async mounted() {
+    await this.gameRequest();
+  },
+  methods: {
+    async gameRequest() {
+      const response = await axios.get(`${process.env.VUE_APP_APIURL}gameinfo`);
+      this.games = response.data.games.results;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
