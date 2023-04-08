56747b8939f14cb8530c278a4f2f5d0d6c3f3003
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2023-04-08 12:39:41 -0300

    layout: adjust gameview layout, with indicators of game tracker situation for each game before and after hover

--- frontend/src/components/GameLayout.vue
+++ frontend/src/components/GameLayout.vue
@@ -2,21 +2,34 @@
   <div class="game-list">
     <div v-for="game in games" :key="game.id" class="game">
       <img :src="game.background_image" :alt="game.name" />
+      <div class="indicators">
+        <span v-if="ownedGames.includes(game.id)" class="fa"
+          ><i class="fas fa-gamepad own"></i
+        ></span>
+        <span v-if="wishListedGames.includes(game.id)" class="fa"
+          ><i class="fas fa-heart wish"></i
+        ></span>
+        <span v-if="favoriteGames.includes(game.id)" class="fa"
+          ><i class="fas fa-star fav"></i
+        ></span>
+      </div>
       <div class="game-hover">
         <p v-if="!logged">
           <router-link to="/login">Login</router-link> or
           <router-link to="/register">Register</router-link> to track your games
         </p>
-        <p v-if="logged">
+        <p class="logged-container" v-if="logged">
           <button
             v-if="ownedGames.includes(game.id)"
             @click="removeOwned(game.id)"
             type="button"
           >
-            Remove from Owned
+            <i class="fas fa-gamepad removeOwned"></i>
+            <p class="button-legend">Remove from owned</p>
           </button>
           <button v-else @click="addOwned(game.id)" type="button">
-            Add as Owned
+            <i class="fas fa-gamepad"></i>
+            <p class="button-legend">Add as owned</p>
           </button>
 
           <button
@@ -26,7 +39,8 @@
             @click="removeWishList(game.id)"
             type="button"
           >
-            Remove from Wishlist
+            <i class="fas fa-heart removeWishlist"></i>
+            <p class="button-legend">Remove from Wishlist</p>
           </button>
           <button
             v-if="
@@ -36,7 +50,8 @@
             @click="addWishList(game.id)"
             type="button"
           >
-            Add to Wishlist
+            <i class="far fa-heart"></i>
+            <p class="button-legend">Add to Wishlist</p>
           </button>
 
           <button
@@ -46,7 +61,8 @@
             @click="removeFavorite(game.id)"
             type="button"
           >
-            Remove from favorites
+            <i class="fas fa-star removeFavorite"></i>
+            <p class="button-legend">Remove favorite</p>
           </button>
 
           <button
@@ -54,16 +70,18 @@
             @click="addFavorite(game.id)"
             type="button"
           >
-            Add as Favorite
+            <i class="far fa-star addFavorite"></i>
+            <p class="button-legend">Add favorite</p>
           </button>
         </p>
         <!-- ajustar essa rota, apenas teste no momento -->
         <router-link
+          class="info-link"
           :to="{
             name: 'specificGame',
             params: { id: game.id },
           }"
-          ><i class="fa fa-info-circle"></i>
+          ><i class="fa fa-info-circle game-info"></i>
         </router-link>
       </div>
       <div class="game-title">{{ game.name }}</div>
@@ -301,23 +319,65 @@ export default {
   margin-bottom: 40px;
   position: relative;
   border-radius: 12px;
+  padding-bottom: 0;
+  transition: all 0.5s ease-in-out;
 }
 
 .game img {
   border-radius: 12px;
   width: 100%;
-  height: 200px;
+  height: 250px;
   object-fit: cover;
-  margin-bottom: 10px;
+  box-shadow: 5px 5px 4px rgba(0, 0, 0, 0.3);
 }
+.indicators {
+  display: flex;
+  align-items: center;
+  gap: 2px;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 2px;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-top-left-radius: 12px;
+  border-bottom-right-radius: 12px;
+}
+.fav {
+  padding: 2px;
+  color: rgba(250, 250, 45, 0.849);
+  font-size: 12px;
+}
+.own {
+  padding: 2px;
 
+  color: rgba(45, 250, 45, 0.849);
+}
+.wish {
+  padding: 2px;
+
+  color: rgba(250, 45, 45, 0.849);
+}
+.game:hover .indicators {
+  display: none;
+}
 .game-title {
   font-size: 16px;
-  font-weight: bold;
-  margin-bottom: 10px;
+  font-weight: bolder;
   text-align: center;
+  position: absolute;
+  bottom: 0;
+  padding: 15px;
+  background-color: rgba(0, 0, 0, 0.25);
+  width: 100%;
+  text-shadow: 2px 2px 4px #000000;
+  border-bottom-left-radius: 12px;
+  border-bottom-right-radius: 12px;
 }
-
+.game:hover .game-title {
+  background-color: transparent;
+}
+/* add hover */
 .game:hover .game-hover {
   display: flex;
   flex-direction: column;
@@ -325,11 +385,15 @@ export default {
   align-items: center;
   gap: 20px;
 }
+.game:hover {
+  transform: scale(1.15);
+  z-index: 100;
+}
 
 .game-hover {
   display: none;
   width: 100%;
-  height: 200px;
+  height: 250px;
   position: absolute;
   top: 0;
   left: 0;
@@ -346,7 +410,6 @@ export default {
 .game-hover p {
   text-align: center;
   margin: 12px;
-  background: rgba(44, 47, 51, 0.75);
   border-radius: 15px;
   padding: 10px;
   border: none;
@@ -359,6 +422,59 @@ export default {
   color: white;
   font-size: 16px;
   font-weight: 500;
+}
+
+/* design dos botões ao estar logado*/
+.game-hover .game-info {
+  text-shadow: 2px 2px 4px #000000;
+  font-size: 30px;
+}
+.game-hover .info-link {
+  padding: 15px;
+  margin-bottom: 50px;
+}
+.game-hover .logged-container {
+  display: flex;
+  gap: 10px;
+  align-content: center;
+  justify-content: center;
+  margin: 0;
+  margin-top: 20px;
+  padding: 0;
+}
+
+.logged-container button {
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  font-size: 10px;
+  margin: 2px;
+  text-shadow: 2px 2px 4px #000000;
+}
+.logged-container .button-legend {
+  font-size: 12px;
+  font-weight: 700;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  text-shadow: 2px 2px 4px #000000;
+}
+
+.addFavorite {
+  background: transparent;
+}
+.removeFavorite {
+  color: rgba(250, 250, 45, 0.849);
+}
+.removeOwned {
+  color: rgba(45, 250, 45, 0.849);
+}
+.removeWishlist {
+  color: rgba(250, 45, 45, 0.849);
 }
 
 /* AJUSTES MOBILE (nem sei se tão bom mas no chrome ficou ok*/
